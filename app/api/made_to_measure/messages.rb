642d45f83fe2c4c23057b3e7913456b4ac16ff3d
Matt Kirk <matt@matthewkirk.com>
2015-07-17 12:36:07 -0700

    Let's make this at_least_one_of

--- app/api/made_to_measure/messages.rb
+++ app/api/made_to_measure/messages.rb
@@ -6,7 +6,9 @@ module MadeToMeasure
     params do
       requires :subject, type: String, desc: 'A subject.'
       optional :text, type: String, desc: 'A text body.'
-      requires :html, type: String, desc: 'An html body.'
+      optional :html, type: String, desc: 'An html body.'
+
+      at_least_one_of :text, :html
     end
     post '/messages' do
       Message.create!({
