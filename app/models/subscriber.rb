ea3d185a4a959f035923628b7112b26bc8fa6245
Christopher M. Hobbs <cmhobbs@member.fsf.org>
2015-09-05 15:23:34 -0500

    Annoate Subscriber

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: subscribers
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Subscriber < ActiveRecord::Base
 end
