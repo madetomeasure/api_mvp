972a9489c06c071e1a756b52cc281750137cb200
Max Justus Spransy <maxjustus@gmail.com>
2015-09-05 11:10:01 -0700

    Add sidekiq web

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'slim-rails'
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
+gem 'sinatra'
 gem 'sidekiq'
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
+require 'sidekiq/web'
+
+Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+  username == 'mtom' && password == 'basic elephantitis swagger'
+end unless Rails.env.development?
+
 Rails.application.routes.draw do
   mount MadeToMeasure::API => '/'
+
+  mount Sidekiq::Web => '/sidekiq'
 
   root :to => "home#index"
 end
