a40d41e4f6094944553874f5aef1f6d22643eb31
Christopher M. Hobbs <cmhobbs@member.fsf.org>
2015-09-05 22:01:13 -0500

    Add from and reply_to

--- spec/workers/message_delivery_worker_spec.rb
+++ spec/workers/message_delivery_worker_spec.rb
@@ -5,24 +5,24 @@ describe MessageDeliveryWorker do
     Sidekiq::Testing.inline! { example.run }
   end
 
-  let!(:message) { Fabricate(:message) }
-  let!(:sub) { Fabricate(:subscriber) }
-  let!(:sub2) { Fabricate(:subscriber) }
+  let!(:message)     { Fabricate(:message) }
+  let!(:subscriber)  { Fabricate(:subscriber) }
+  let!(:subscriber2) { Fabricate(:subscriber) }
 
   it 'delivers message to each subscriber', integration: true do
     described_class.new.perform(message.id)
-    d = ActionMailer::Base.deliveries
-    expect(d.first.to).to eql([sub.email])
-    expect(d.last.to).to eql([sub2.email])
+    deliveries = ActionMailer::Base.deliveries
+    expect(deliveries.first.to).to eql([subscriber.email])
+    expect(deliveries.last.to).to eql([subscriber2.email])
 
-    msg = d.first.to_s
-    expect(msg).to include(message.html_body)
-    expect(msg).to include(message.text_body)
+    first_message = deliveries.first.to_s
+    expect(first_message).to include(message.html_body)
+    expect(first_message).to include(message.text_body)
   end
 
   it 'creates delivery for each subscriber', integration: true do
     described_class.new.perform(message.id)
-    expect(sub.deliveries.first.message).to eql(message)
-    expect(sub.deliveries.last.message).to eql(message)
+    expect(subscriber.deliveries.first.message).to eql(message)
+    expect(subscriber.deliveries.last.message).to eql(message)
   end
 end
