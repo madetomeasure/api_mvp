2ace9d6732da2af3f68ed19f5997aaa6dadf9e0b
Max Justus Spransy <maxjustus@gmail.com>
2015-09-05 21:57:43 -0700

    Clarify RenderMessage class

--- app/services/render_message.rb
+++ app/services/render_message.rb
@@ -1,16 +1,18 @@
+# Provides methods for rendering mustache templates
+# in messages using subscriber data
 class RenderMessage
-  attr_reader :template, :subscriber
-  def initialize(template, subscriber)
-    @template = template
+  attr_reader :message, :subscriber
+  def initialize(message, subscriber)
+    @message = message
     @subscriber = subscriber
   end
 
   def html
-    presenter.render(mustache_template(:html, template.html_body))
+    presenter.render(mustache_template(:html, message.html_body))
   end
 
   def text
-    presenter.render(mustache_template(:text, template.text_body))
+    presenter.render(mustache_template(:text, message.text_body))
   end
 
   private
@@ -21,6 +23,6 @@ class RenderMessage
 
   def mustache_template(type, raw_template)
     return '' if raw_template.blank?
-    MustacheTemplateCache.fetch("#{type}_#{template.id}", raw_template)
+    MustacheTemplateCache.fetch("#{type}_#{message.id}", raw_template)
   end
 end
